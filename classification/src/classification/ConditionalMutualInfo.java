d8c4dc267f6e00d2959ca6def564d150b0d2bcd5
Monica Thornton <monicasuecooper@hotmail.com>
2016-11-18 02:22:19 -0700

    CMI skeleton.

---
+++ classification/src/classification/ConditionalMutualInfo.java
@@ -0,0 +1,18 @@
+package classification;
+
+public class ConditionalMutualInfo {
+
+	public ConditionalMutualInfo(String attI, String attJ, String class) {
+		this.data = data;
+	}
+	
+	
+	/*
+	 *  Measure used in tree-augmented naive Bayes (TAN) algorithm. Conditional mutual information (CMI) helps 
+	 *  to determines which dependencies to add back into the network (by estimating the amount entropy can be
+	 *  reduced through the addition of a dependency relationship. 
+	 */
+	
+	
+	
+}
