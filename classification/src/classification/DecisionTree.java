2562bcd791461b8f18b9d5fb8a2214a77eec8ba9
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-11-18 00:09:59 -0700

    Decision Tree building done

--- classification/src/classification/DecisionTree.java
+++ classification/src/classification/DecisionTree.java
@@ -7,16 +7,24 @@ import java.util.logging.Level;
 
 public class DecisionTree extends Algorithm {
 	ArrayList<String[]> trainset;
+	ArrayList<String[]> testset;
 	ArrayList<String[]> valset;
+	double valratio;
 	Tree tree;
 	ArrayList<String> classlabels;
-	public DecisionTree(ArrayList<String[]> data){
-		
+
+	public DecisionTree(ArrayList<String[]> trainset, ArrayList<String[]> testset, double valratio) {
+		this.trainset = trainset;
+		this.testset = testset;
+		this.valratio = valratio;
+		train(trainset);
+
 	}
+
 	void test(ArrayList data) {
 
 	}
-	
+
 	void train(ArrayList data) {
 		super.get_logger().log(Level.INFO, "ID3 Training started");
 		trainset = (ArrayList<String[]>) (ArrayList<?>) (data);
@@ -29,7 +37,7 @@ public class DecisionTree extends Algorithm {
 		}
 		super.get_logger().log(Level.INFO, "Class labels set created");
 		// create validation set
-		valset = createValSet(trainset, 0);
+		valset = createValSet(trainset, valratio);
 		super.get_logger().log(Level.INFO, "Validation set created");
 		// create root node
 		DecisionTreeNode root = new DecisionTreeNode(trainset, -1, 0);
@@ -44,6 +52,12 @@ public class DecisionTree extends Algorithm {
 		// call treeBuilder on data, attribute array, root
 		treeBuilder(trainset, attributes, root);
 		super.get_logger().log(Level.INFO, "Tree Built");
+		ArrayList<TreeNode> t = tree.getTree();
+		for (TreeNode i : t) {
+			DecisionTreeNode j = (DecisionTreeNode) i;
+			System.out.println("Node " + j.getLocindex() + " has parent " + j.getRootindex()
+					+ " and splits on attribute " + j.attribute);
+		}
 		// call pruning
 
 	}
@@ -54,8 +68,11 @@ public class DecisionTree extends Algorithm {
 		HashMap<String, Integer> countmap = countAllClasses(subset);
 		super.get_logger().log(Level.INFO, "Hashmap of class numbers created");
 		// if all examples have the same class
+		System.out.println(subset.size());
+		System.out.println(countmap.toString());
 		if (countmap.size() == 1) {
-			super.get_logger().log(Level.INFO, "All examples of Node Number " +  tree.getTreeSize() + " are the same class");
+			super.get_logger().log(Level.INFO,
+					"All examples of Node Number " + tree.getTreeSize() + " are the same class");
 			// System.out.println(team1.keySet().toArray()[0]);
 			String key = "";
 			for (Map.Entry<String, Integer> entry : countmap.entrySet()) {
@@ -65,11 +82,11 @@ public class DecisionTree extends Algorithm {
 			// remove all children for parent
 			parent.label = key;
 			parent.children = new ArrayList<TreeNode>();
-			
+
 		} else if (attributes.size() < 1) {
 			// else if attributes is empty
-			super.get_logger().log(Level.INFO, "Attributes for Node Number " +  tree.getTreeSize() + " are empty");
-			
+			super.get_logger().log(Level.INFO, "Attributes for Node Number " + tree.getTreeSize() + " are empty");
+
 			HashMap.Entry<String, Integer> maxEntry = null;
 
 			for (HashMap.Entry<String, Integer> entry : countmap.entrySet()) {
@@ -84,8 +101,8 @@ public class DecisionTree extends Algorithm {
 		}
 		// else if subset.size == 1
 		else if (subset.size() == 1) {
-			super.get_logger().log(Level.INFO, "Only one example for Node Number " +  tree.getTreeSize());
-			
+			super.get_logger().log(Level.INFO, "Only one example for Node Number " + tree.getTreeSize());
+
 			// parent.label = class label of example
 			parent.label = subset.get(0)[subset.get(0).length - 1];
 			// remove all children for parent
@@ -110,69 +127,78 @@ public class DecisionTree extends Algorithm {
 		// }
 		// else
 		else {
-			super.get_logger().log(Level.INFO, "Expanding tree at " +  tree.getTreeSize() + "");
-			
-			//calc Gain ratio:
+			super.get_logger().log(Level.INFO, "Expanding tree at " + tree.getTreeSize() + "");
+
+			// calc Gain ratio:
 			HashMap<Integer, HashMap<Double, HashMap<String, ArrayList<String[]>>>> outer = new HashMap<Integer, HashMap<Double, HashMap<String, ArrayList<String[]>>>>();
-			//Nested Hashmap!
-			//map of attribute -> map of attribute values -> map of subset of data -> gain ratio
-			for(Integer i: attributes){//for each attribute
+			// Nested Hashmap!
+			// map of attribute -> map of attribute values -> map of subset of
+			// data -> gain ratio
+			for (Integer i : attributes) {// for each attribute
 				HashMap<Double, HashMap<String, ArrayList<String[]>>> inner = new HashMap<Double, HashMap<String, ArrayList<String[]>>>();
-				HashMap<String, ArrayList<String[]>> russiandoll= new HashMap<String, ArrayList<String[]>>();
-				for(String[] ex : subset){//for each example in the dataset
-					if(!inner.containsKey(ex[i])){
-						
-						
-						//create subsubset of data with value ex[i]
+				HashMap<String, ArrayList<String[]>> russiandoll = new HashMap<String, ArrayList<String[]>>();
+				for (String[] ex : subset) {// for each example in the dataset
+					if (!inner.containsKey(ex[i])) {
+
+						// create subsubset of data with value ex[i]
 						ArrayList<String[]> subsub = new ArrayList<String[]>();
-						for(String[] ex2: subset){
-							if(ex2[i] == ex[i]){
+						for (String[] ex2 : subset) {
+							if (ex2[i].equals(ex[i])) {
 								subsub.add(ex2);
 							}
 						}
-						
-						//calculate gain ratio for subsubset
+
+						// calculate gain ratio for subsubset
 						russiandoll.put(ex[i], subsub);
-						
+
 					}
 				}
-				double g = calcGainRatio(russiandoll, i);
-				super.get_logger().log(Level.INFO, tree.getTreeSize() + " : Attribute " + i + " : Gain Ratio " + g);
+				double g = calcGainRatio(subset, i);
+				// System.out.println(tree.getTreeSize() + " : Attribute " + i +
+				// " : Gain Ratio " + g);
+				// super.get_logger().log(Level.INFO, tree.getTreeSize() + " :
+				// Attribute " + i + " : Gain Ratio " + g);
 				inner.put(g, russiandoll);
 				outer.put(i, inner);
 			}
 			super.get_logger().log(Level.INFO, "Really cool HashMap created!");
-			
-			//find max gain
+
+			// find max gain
 			Map.Entry<Integer, HashMap<Double, HashMap<String, ArrayList<String[]>>>> maxEntry = null;
 
-			for (Map.Entry<Integer, HashMap<Double, HashMap<String,ArrayList<String[]>>>> entry : outer.entrySet())
-			{
-				double e = (double)entry.getValue().keySet().toArray()[0];
-				double m = (double)maxEntry.getValue().keySet().toArray()[0];
-			    if (maxEntry == null || e > m)
-			    {
-			        maxEntry = entry;
-			    }
-			}
-			super.get_logger().log(Level.INFO, "Max Gain Ratio is : " + maxEntry.getValue().keySet().toArray()[0]);
-			//set as attribute
-			parent.attribute = maxEntry.getKey();
-			attributes.remove(attributes.indexOf(maxEntry.getKey()));
-			//take keys for russiandoll as split values
-			HashMap<String,ArrayList<String[]>> russiandoll = (HashMap<String,ArrayList<String[]>>)maxEntry.getValue().values().toArray()[0];
-			parent.splitvalues = new ArrayList<String>(russiandoll.keySet());
-			//create children
-			for(String v : parent.splitvalues){
-				if(!russiandoll.get(v).isEmpty()){
-					TreeNode child = new DecisionTreeNode(russiandoll.get(v), parent.locindex, tree.getTreeSize());
-					//add children to tree and parent's children set
-					parent.children.add(child);
-					tree.addNode(child);
-					treeBuilder(russiandoll.get(v), attributes, (DecisionTreeNode)child);
+			for (Map.Entry<Integer, HashMap<Double, HashMap<String, ArrayList<String[]>>>> entry : outer.entrySet()) {
+				double e = (double) entry.getValue().keySet().toArray()[0];
+
+				if (maxEntry == null) {
+					maxEntry = entry;
+				} else {
+					double m = (double) entry.getValue().keySet().toArray()[0];
+					if (e > m)
+
+					{
+						maxEntry = entry;
+					}
 				}
 			}
-			
+			super.get_logger().log(Level.INFO, "Max Gain Ratio is : " + maxEntry.getValue().keySet().toArray()[0]);
+			// set as attribute
+			// if(maxEntry.getValue().keySet().toArray()[0])
+			parent.attribute = maxEntry.getKey();
+			attributes.remove(attributes.indexOf(maxEntry.getKey()));
+			// take keys for russiandoll as split values
+			HashMap<String, ArrayList<String[]>> russiandoll = (HashMap<String, ArrayList<String[]>>) maxEntry
+					.getValue().values().toArray()[0];
+			parent.splitvalues = new ArrayList<String>(russiandoll.keySet());
+			// create children
+			for (String v : parent.splitvalues) {
+				if (!russiandoll.get(v).isEmpty()) {
+					TreeNode child = new DecisionTreeNode(russiandoll.get(v), parent.locindex, tree.getTreeSize());
+					// add children to tree and parent's children set
+					parent.children.add(child);
+					tree.addNode(child);
+					treeBuilder(russiandoll.get(v), attributes, (DecisionTreeNode) child);
+				}
+			}
 
 		}
 
@@ -187,45 +213,165 @@ public class DecisionTree extends Algorithm {
 		return data;
 	}
 
-	protected double calcGainRatio(HashMap<String, ArrayList<String[]>> subsubsets, int attribute) {
-		
-		return 1;
+	protected double calcGainRatio(ArrayList<String[]> subset, int attribute) {
+		double g = calcGain(subset, attribute);
+		double iv = calcIV(subset, attribute);
+		super.get_logger().log(Level.INFO, "Calculating gain ratio for attribute " + attribute + " : " + g / iv);
+		// for(String[] ex : subset){
+		// System.out.print("example number " + subset.indexOf(ex) + " : ");
+		// for(String feat : ex){
+		// System.out.print(feat + ", ");
+		// }
+		// System.out.println();
+		// }
+
+		return g / iv;
 	}
 
-	private double calcEntropy(ArrayList<String[]> subsubset, int attribute) {//probability of class
-		
-		return 1;
+	private double calcEntropy(ArrayList<String[]> subsubset) {// probability of
+																// class
+		double citok = subsubset.size();
+		// System.out.println(subsubset.size());
+		HashMap<String, Integer> cis = countAllClasses(subsubset);
+
+		double entropy = 0;
+		for (Integer i : cis.values()) {
+			if (i != 0) {
+				double e = i / citok;
+				entropy += ((e) * (Math.log(e) / Math.log(2)));
+				// System.out.println("incremental entropy: " +i + " : " +
+				// entropy);
+			}
+		}
+		// System.out.println();
+		return -1 * entropy;
 	}
 
 	private double calcGain(ArrayList<String[]> subsubset, int attribute) {
-		return 1;
+		double citok = subsubset.size();
+		double gain = 0;
+		double entropy = calcEntropy(subsubset);
+		HashMap<String, Integer> countmap = countInstances(subsubset, attribute);
+		HashMap<String, ArrayList<String[]>> subsetmap = subdivideData(subsubset, attribute);
+		// for(String val : subsetmap.keySet()){
+		// for(String[] example : subsetmap.get(val)){
+		// for(String feat : example){
+		// System.out.print(feat + ",");
+		// }
+		// System.out.println();
+		// }
+		// }
+		for (String val : countmap.keySet()) {
+
+			double subentropy = calcEntropy(subsetmap.get(val));
+			// System.out.println("subentropy : " + subentropy);
+			gain += (countmap.get(val) / citok);// *subentropy;
+			// System.out.println("gain for " + val + " : " +gain);
+		}
+		// System.out.println("entropy : " + entropy);
+		return entropy - gain;
 	}
 
-	private double calcIV(ArrayList<String[]> subsubset, int attribute) {//probability of attribute value
-		return 1;
+	private double calcIV(ArrayList<String[]> subsubset, int attribute) {// probability
+																			// of
+																			// attribute
+																			// value
+		HashMap<String, Integer> countmap = countInstances(subsubset, attribute);
+		double citok = subsubset.size();
+
+		double intrinsicVal = 0;
+		for (Integer i : countmap.values()) {
+			if (i != 0) {
+				// double blergh = i/citok;
+				// System.out.println(i + " : " + citok + " : " + (1/citok) + "
+				// : " + Math.log(i/citok) + " : " +Math.log(blergh));
+				intrinsicVal += ((i / citok) * (Math.log(i / citok) / Math.log(2)));
+				// System.out.println("iv: " + intrinsicVal);
+			}
+		}
+		if (intrinsicVal == 0) {
+			return intrinsicVal;
+		}
+		return -1 * intrinsicVal;
 	}
 
 	private int countclass(String classlabel, ArrayList<String[]> data) {
+		// System.out.println("data size: " + data.size());
 		int classloc = data.get(0).length - 1;
 		int count = 0;
 		for (String[] i : data) {
-			if (classlabel == i[classloc]) {
+			if (classlabel.equals(i[classloc])) {
 				count++;
 			}
 		}
 		return count;
 	}
 
-	private HashMap<String, Integer> countAllClasses(ArrayList<String[]> subset) {
+	private HashMap<String, Integer> countInstances(ArrayList<String[]> subset, int attribute) {
 		HashMap<String, Integer> countmap = new HashMap<String, Integer>();
-		for (int i = 0; i < classlabels.size(); i++) {
-			if (!countmap.containsKey(classlabels.get(i))) {
-				countmap.put(classlabels.get(i), countclass(classlabels.get(i), subset));
-			} else {
-				countmap.remove(classlabels.get(i));
-				countmap.put(classlabels.get(i), countclass(classlabels.get(i), subset));
+		for (String[] ex : subset) {
+			if (!countmap.containsKey(ex[attribute])) {
+				int count = 0;
+				for (String[] ex2 : subset) {
+					if (ex[attribute].equals(ex2[attribute])) {
+						count++;
+					}
+				}
+				countmap.put(ex[attribute], count);
 			}
 		}
 		return countmap;
+	}
+
+	private HashMap<String, Integer> countAllClasses(ArrayList<String[]> subset) {
+
+		HashMap<String, Integer> countmap = new HashMap<String, Integer>();
+		for (int i = 0; i < classlabels.size(); i++) {
+			if (!countmap.containsKey(classlabels.get(i))) {
+				int c = countclass(classlabels.get(i), subset);
+				if (c != 0) {
+					countmap.put(classlabels.get(i), c);
+				}
+			}
+			// else {
+			// countmap.remove(classlabels.get(i));
+			// countmap.put(classlabels.get(i), countclass(classlabels.get(i),
+			// subset));
+			// }
+		}
+		return countmap;
+	}
+
+	private HashMap<String, ArrayList<String[]>> subdivideData(ArrayList<String[]> data, int attribute) {
+
+		HashMap<String, ArrayList<String[]>> countmap = new HashMap<String, ArrayList<String[]>>();
+		for (String[] ex : data) {
+			if (!countmap.containsKey(ex[attribute])) {
+				ArrayList<String[]> subset = new ArrayList<String[]>();
+				for (String[] ex2 : data) {
+					if (ex[attribute].equals(ex2[attribute])) {
+						subset.add(ex2);
+					}
+					// System.out.println("subdivided set size : " +
+					// subset.size() + " at " + data.indexOf(ex) + " : " +
+					// ex[attribute] + " and " + data.indexOf(ex2) + " : " +
+					// ex2[attribute] + " and attribute " + attribute);
+				}
+				// System.out.println("subdivided set size : " + subset.size() +
+				// " at " + data.indexOf(ex) + " and " + data.indexOf(ex2) + "
+				// and attribute " + attribute);
+				countmap.put(ex[attribute], subset);
+			}
+		}
+		return countmap;
+	}
+
+	private void printTree(boolean log) {
+		if (log) {
+
+		} else {
+			TreeNode root = tree.getRoot();
+
+		}
 	}
 }
--- classification/src/classification/RunModels.java
+++ classification/src/classification/RunModels.java
@@ -111,7 +111,7 @@ public class RunModels {
     		Algorithm tan = new TreeAugNB(); 
     	} else if (choice.equals("id3")) {	
     		System.out.println("Classifying data using the Iterative Dichotomiser 3"); 
-    		Algorithm id3 = new DecisionTree();    		
+    		Algorithm id3 = new DecisionTree(train1, test1, 0);    		
     	} else {
     		// user chose to exit the program or typed their choice incorrectly
     		System.out.println("Exiting program.");
